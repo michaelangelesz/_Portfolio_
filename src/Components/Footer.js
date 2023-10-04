de7cacb01bf0bd7d09232eb38554a68222b35c21
michaelangelesz <125311967+michaelangelesz@users.noreply.github.com>
2023-10-04 15:27:25 -0400

    fix footer alignments between viewport sizes

--- src/Components/Footer.js
+++ src/Components/Footer.js
@@ -9,21 +9,21 @@ export const Footer = () => {
     <footer className="footer">
       <Container>
         <Row className="align-items-center">
-          <Col size={12} sm={6} className="logo">
+          <Col xs={12} sm={6} className="footer-logo text-center text-sm-start">
             <img src={logo} alt="WebDesigns by Mike" />
-            <h3>
+            <h5>
             ✦ WebDesigns by Mike ✦
-            </h3>
+            </h5>
           </Col>
-          <Col size={12} sm={6} className="text-center text-sm-end">
+          <Col xs={12} sm={6} className="text-center text-sm-end">
             <div className="social-icon">
               <a href="https://www.linkedin.com/in/michael-m-weisphal/"><img src={linkedIn} alt="Icon" /></a>
               <a href="https://github.com/michaelangelesz"><img src={github} alt="Icon" /></a>
               <a href="https://www.instagram.com/michaelangelesz/"><img src={instagram} alt="Icon" /></a>
             </div>
             <p className="copyright">
-            ✦ WebDesigns by Mike ✦ 2020-
-                {new Date().getFullYear()} ✦
+            Mike M. Weisphal © 2020-
+                {new Date().getFullYear()}
             </p>
           </Col>
         </Row>
