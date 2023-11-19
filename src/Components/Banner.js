13bb52f8de2144aa7b0e5f714cb26d458d6a6c17
michaelangelesz <125311967+michaelangelesz@users.noreply.github.com>
2023-11-19 11:20:07 -0500

    update banner image

--- src/Components/Banner.js
+++ src/Components/Banner.js
@@ -1,5 +1,5 @@
 import { Container, Row, Col } from "react-bootstrap"
-import headerImg from "../assets/images/me.jpeg"
+import headerImg from "../assets/images/me2.jpeg"
 
 export const Banner = () => {
   return (
